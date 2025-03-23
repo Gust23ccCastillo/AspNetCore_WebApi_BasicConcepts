e684f6837d9160374c80befeff73beb9cfe65690
Kenneth Castillo <@Gust23ccCastillo>
2025-03-23 17:38:46 -0600

    Ocultando archivos y carpetas en GitHub mediante .gitignore

--- BookingApplication.Services/obj/Debug/net6.0/BookingApplication.Services.AssemblyInfo.cs
+++ BookingApplication.Services/obj/Debug/net6.0/BookingApplication.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingApplication.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab2176e9983b9884222aa074881483f1201355de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8340606af24a82d4b647b04fae5a583f61e49656")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingApplication.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingApplication.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
